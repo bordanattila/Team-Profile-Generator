3601ae15745ff64f573a9060929b8e2285dd4e46
bordanattila <bordan_attila@hotmail.com>
2022-09-10 11:11:27 -0400

    organize directory structure

---
+++ dist/script.js
@@ -0,0 +1,21 @@
+// import managerData from "./employees.json" assert { type: "JSON"};
+
+const {theManager} = require(".././lib/menu")
+
+function create (whatever) {
+    console.log("this is whatever")
+    console.log(whatever)
+    // console.log(whatever.Manager[0].name)
+    // // e.preventDefault();
+    // const div = $("#test");
+    // const manager = $("<h3>");
+    // // const mID = $("<p>");
+    // // const mEmail = $("<p>");
+    // // const mOffice = $("<p>");
+    // manager.text(whatever.Manager.name)
+    // div.append(manager)
+}
+// create(theManager);
+
+
+module.exports = create;
--- index.js
+++ index.js
@@ -1,41 +1,13 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
 
-const Manager = require("./employee");
-const Engineer = require("./employee");
-const Intern = require("./employee");
-const menu = require("./menu")
+const {menu} = require("./lib/menu");
+const create = require("./dist/script");
 
-inquirer
-.prompt([
-    {
-        type: "input",
-        name: "manager",
-        message: "Enter team manager's name: ",
-    },
-    {
-        type: "input",
-        name: "managerID",
-        message: "Enter team manager's ID: ",
-    },
-    {
-        type: "input",
-        name: "manageremail",
-        message: "Enter team manager's email: ",
-    },
-    {
-        type: "input",
-        name: "officenumber",
-        message: "Enter team manager's office number: ",
-    }
-])
+const promise1 = new Promise((resolve, reject) => {
+    menu();
+
+})
+
+Promise.all([promise1])
 .then((response) => {
-    const mn = new Manager(response.manager, response.managerID, response.manageremail, response.officenumber);
-    const on = new Manager(response.officenumber);
-    console.log(mn);
-    console.log(Manager.additional);
-    const eng = new Engineer(response.manager, response.managerID, response.manageremail, response.officenumber);
-    console.log("Engineer");
-    console.log(eng);
-    menu()
+    create()
 })
